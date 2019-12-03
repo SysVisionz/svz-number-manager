cc619ffae9e22ce771c4148693bd2fb553150af3
SysVisionz <colin@sysvisionz.com>
2019-12-03 15:36:53 -0700

    Finalized a lot of the class, built frame of readme

--- src/index.js
+++ src/index.js
@@ -1,203 +1,198 @@
-export default class NumberManager {
-	constructor(number, exact){
-		this.number = number;
-		this.exact = exact;
-	} 
+numType = num => {
+	if (typeof num === 'number'){
+		return 'number';
+	}
+	if (typeof num === 'string' && String(num).match(RegExp('\.', 'g')).length > 1){
+		return 'string'
+	}
+	throw "TypeError: num must be a Number or String including a maximum of 1 . character"
+}
 
-	//toPhone converts a number or string to a US phone number format.
-	static toPhone = (number, style = "(###) ###-####") => {
-		if (!['number', 'string'].includes(typeof number)){
-			throw "TypeError: number must be a Number or String."
-		}
-		if (typeof number === 'number'){
-			number = String(number)
-		}
-		number = number.match( /[0-9]/g ).join('')
-		style = style.split('');
-		number = number.split('');
-		index = 0;
-		if (style.filter(e => e === '#').length !== number.length){
-			throw "SyntaxError: The numbers required by style do not match the digits provided in number."
-		}
-		for (const i in style){
-			if (style[i] === '#'){
-				style[i] = number[index]
-				index++;
+charMap = (string, callback) => string.split('').map(callback);
+
+export default class NumberManager {
+	constructor(vals){
+		for (const i in vals){
+			if (['phoneStyle', 'size', 'max', 'min', 'country', 'num'].includes(i)) {
+				this[i] = vals[i]
 			}
 		}
-		return style.join('');
-	}
-
-	toPhone = style => NumberManager.toPhone(this.number, nationCode)
-
-	//toDollars returns either the NumberMan's number or an inputted number to currency format
-	static toDollars = number => {
-		if (!['number', 'string'].includes(typeof number)){
-			throw "TypeError: number must be a number or string."
-		}
-		return "$" + Number(typeof number === 'number' ? number : number.match( /[0-9]|\./g ).join('')).toFixed(2);
-	}
-
-	static prefixZeroes = (number, length) => {
-		if (!['string', 'number'].includes(typeof number)){
-			throw "TypeError: number must be a number or string";
-		}
-		if (typeof number === 'number'){
-			number = number.toString();
-		}
-		for (let i = 0; i < number.length; i++){
-			number = '0'+number;
+		this.countriesVal = {
+			US: {
+				currency: {
+					code:'USD',
+					format: "$*.##"
+				},
+				phone: "(###) ###-####"
+			}
 		}
 	}
 
-	toDollars = () => NumberManager.toDollars(this.number);
+//class variable setters and getters
 
-	//addZeroes adds zeroes as a prefix or as decimal places until the test evaluates as false.
-	static size = (number, length, maxLen) => {
-		length = NumberManager.confine(typeof length === 'string' ? length.length : length, maxLen);
-		number = typeof number === 'string' ? number : new Number(number).toString()
-		if (number.length > maxLen){
-			throw "SyntaxError: number exceeds the maximum length specified."
+	set countries(){ throw "Exception: countries is a read-only variable."}
+
+	get countries(){ return this.countriesVal }
+
+	set country(val){ 
+		if (Object.keys(this.countries).includes(val){
+			this.countryVal = val;
 		}
-		return number.length < length ? Array(length - number.length).fill(0).join('') + number : number
+		else {
+			throw `Exception: ${val} is not a currently implemented country code.`
+		}
 	}
 
-	set size (length){ this.number = NumberManager.prefixZeroes(this.number, length, this.maxLength)}
+	get country(){ return this.countryVal || 'US'}
 
-	get size (){ return this.number.length }
+	set currencyStyle(val){ this.currencyStyleVal = val}
 
-	static absRound(number, direction = 'round', places = 0, absolute){
-		number = number*Math.pow(10, places);
-		if (absolute || ~NumberManager.positive(number)){
-			return absolute ? Math.abs(Math[direction](number))/Math.pow(10, places) : Math[direction](number)/Math.pow(10, places)
+	get currencyStyle(){return this.currencyStyleVal || this.countries[this.country].currency.format }
+
+	set max(val){
+		if (this.min && maxVal < this.min){
+			throw `Exception: ${max} is lower than ${this.min}. NumberManager cannot have a lower max than min.`;
+		}
+		this.maxVal = maxVal;
+	}
+
+	get max(){ return this.maxVal}
+
+	set min(val){
+		if (this.max && minVal > this.max){
+			throw `Exception: ${min} is larger than ${this.max}. NumberManager cannot have a larger min than max.`;
+		}
+		this.minVal = minVal;
+	}
+
+	get min(){ return this.minVal }
+
+	set num(val) { this.numVal = this.setSize(this.confine(val))}
+
+	get num() { return this.numVal }
+
+	set phoneStyle(val){ this.phoneStyleVal = val }
+
+	get phoneStyle(){return this.phoneStyleVal || this.countries[this.country].phone}
+
+	set size (size) { this.sizeVal = this.sizeOf(size) }
+
+	get size(){ return this.sizeVal}
+
+//methods
+
+	absRound = (num = this.num, direction = 'ceil', places = 0, absolute) => {
+		num = num*Math.pow(10, places);
+		if (absolute || this.positive(num)){
+			return absolute ? Math.abs(Math[direction](num))/Math.pow(10, places) : Math[direction](num)/Math.pow(10, places)
 		}
 		else {
 			switch (direction){
 				case 'ceil':
-					return Math.floor(number)/Math.pow(10, places);
+					return Math.floor(num)/Math.pow(10, places);
+				case 'round':
+					return Math.round(num)/Math.pow(10, places);
 				case 'floor':
-					return Math.ceil(number)/Math.pow(10, places);
+					return Math.ceil(num)/Math.pow(10, places);
 				default:
-					return Math.round(number)/Math.pow(10, places);
+					throw `Exception: ${direction} is not valid for absRound. Use ceil, floor, or round.`
 			}
 		}
 	}
 
-	static confine = (number, max, min) =>  {
-		if (max > min){
-			throw "SyntaxError: max must be greater than min."
-		}
-		if (max){
-			number = Math.min(number, max);
-		}
-		if (min){
-			number = Math.max(number, min);
-		}
-		return number
-	}
-
-	static positive = (number, asDirection) => {
-		if (typeof number)
-		if (number > 0){
-			return asDirection ? 1 : true;
-		}
-		if (number < 0){
-			return asDirection ? -1 : false
-		}
-	}
-
-	positive = asDirection => NumberManager.positive(this.number, asDirection);
-
-
-	set length (len) {
-		this.maxDigits = typeof digits === 'string' ? NumberManager.sizeOf(digits, this.zeroesPrefix) : digits;
-		this.number = this.number;
-	}
-
-	set digits (dig) {
-		this.maxDigits = typeof dig === 'string' ? dig.length : dig;
-	}
-
-	static sizeOf = (number, zeroesPrefix) => {
-		number = typeof number === 'string' ? number : new Number(number).toString();
-		if (zeroesPrefix){
-			return number.length;
-		}
-		else {
-			return number.includes('.')
-				? number.substring((''+number).indexOf('.')+1).length 
-				: 0;
-		}
-	}
-	
-	static addZero = (number, zeroesPrefix) => {
-		if (zeroesPrefix){
-			return '0' + number 
-		}
-		else{
-			number = typeof number === 'string' ? number : new Number(number).toString()
-			return number + (number.includes('.') ? '0' : '.0')
-		}
-	}
-
-	capDigits = (number, digits = this.maxDigits) => Math.round(Number.parseFloat(number)*Math.pow(10,digits))/Math.pow(10,digits);
-
-	set number (numberValue) {
-		//ensure that the number's value does not exceed the maximum trailing digits set for this NumberMan.
-		if (!['number', 'string'].includes(typeof numberValue)){
-			throw "TypeError: NumberManager's number variable must be a Number or String";
-		}
-		if (this.maxDigits && this.trailingDigits(numberValue) > this.maxDigits) {
-			this.numberVal = ''+numberValue;
-			this.exactPlaces(this.maxDigits)
-		} else {
-			this.numberVal = ''+numberValue;
-			if (this.exact){
-				this.addZeroes(this.maxDigits)
-			}
-		}
-	}
-
-	get number(){
-		return this.numberVal
-	}
-
-	// toDigits accepts a string to test against, an array of values to test against or a number of digits
-	// and either adds zeroes as prefixes or zeroes as decimal places.
-	toDigits = (digitsOrNumberArray, trailing = true) => {
-		const {addZeroes, trailingDigits} = this;
-		const newLength = (current, test) => {
-			const digits = trailing ? trailingDigits(test) : test.length;
-			return Math.max(digits, current);
-		}
-		let toLength = trailing ? trailingDigits() : this.number.length;
-		if (typeof digitsOrNumberArray === 'object'){
-			for (const i in digitsOrNumberArray){
-				toLength = newLength(toLength, ''+digitsOrNumberArray[i])
-			}
-		}
-		else if(typeof digitsOrNumberArray === 'string'){
-			toLength =  newLength(toLength, digitsOrNumberArray);
-		}
-		else if(typeof digitsOrNumberArray === 'number'){
-			toLength = Math.max(toLength, digitsOrNumberArray);
-		}
-		addZeroes(toLength, trailing);
-		return this.number;
-	}
-
-	static calcAdditive = (number, maxVal) => {
+	calcAdditive = (num = this.num, maxVal = this.max) => {
 		let i = 0;
 		let calcTotal = 0;
-		while (Math.abs(calcTotal) < Math.abs(number)) {
+		while (Math.abs(calcTotal) < Math.abs(num)) {
 			i++;
-			const addTo = maxVal && i > maxVal ? maxVal*Number.positive(number) : Number.positive(number)*i;
+			const addTo = maxVal && i > maxVal ? maxVal*Number.positive(num) : Number.positive(num)*i;
 			calcTotal += addTo;
 		}
 		return i;
 	}
 
-	calcAdditive = maxVal => {
-		NumberManager.calcAdditive(this.number, maxVal);
+	confine = (num = this.num, max = this.max, min = this.min) => Math.min(max, Math.max(min, num))
+
+	decimalPlaces = (num = this.num) => String(num).split('.')[1].length;
+
+	positive =  (num = this.num, asDirection) => {
+		if (num > 0){
+			return asDirection ? 1 : true;
+		}
+		if (num < 0){
+			return asDirection ? -1 : false
+		}
+	}
+
+	rand = (max = this.max || 99, min = this.min || 0) => Math.floor(min+Math.random()*(max - min + 1))
+
+	setSize = (num = this.num, length = this.size) => {
+		testInvalid(num);
+		if (!length){
+			return this.sizeOf(num);
+		}
+		length = this.size(length)
+		if (typeof num === 'string'){
+			return num.length < length ? Array(length - num.length).fill(0).join('') + num: num
+		}
+		else {
+			return Number(num).toFixed(length);
+		}
+	}
+
+	sizeOf = (num = this.num) => {
+		if ( typeof num === 'string'){
+			if (numVal.includes('.')){
+				return numVal.split('.')[1].length;
+			}
+			return numVal.length;
+		}
+		else if (typeof num === 'number') {
+			return number
+		}
+	}
+
+	toCurrency = (num, style = this.currencyStyle) => {
+		if (numType(num) === 'string'){
+			num = num.match( /[0-9]|\./g ).join('')
+		}
+		const digits = this.decimalPlaces(style)
+		num = Number(num).toFixed(digits)
+		let splitNum = String(num).split('.');
+		splitNum = [splitNum[0].split(''), splitNum[1] ? splitNum[1].split('') : []]
+		return charMap(style, chara => {
+			switch(chara){
+				case '#':
+					return splitNum[0].length > 0 ? splitNum[0].splice('') : splitNum[1] && splitNum[1].length > 0 ? splitNum[1].splice(0,1) : 0;
+				case '*':
+					if (splitNum[0].length > 0){
+						const ret = splitNum[0].join('')
+						splitNum[0] = [];
+						return ret;
+					}
+					splitNum[1] = [];
+					return splitNum[1].join('');
+				default:
+					return chara;
+			}
+		}).join('');
+	}
+
+	toPhone = (num = this.num, style = this.phoneStyle ) => {
+		num = String(num)
+		num = num.toUpperCase().match( /[A-Z0-9]/g ).join('')
+		style = style.split('');
+		num= num.split('');
+		index = 0;
+		if (style.filter(e => e === '#').length !== num.length){
+			throw "SyntaxError: The nums required by style do not match the digits provided in num."
+		}
+		for (const i in style){
+			if (style[i] === '#'){
+				style[i] = num[index]
+				index++;
+			}
+		}
+		return style.join('');
 	}
 }
